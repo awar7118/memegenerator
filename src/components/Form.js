6f7ed9fa1a01c13b68bbd6acf790fe435a68b48d
Ahmed Warsama <ahmedwarsama@Ahmeds-MacBook-Air.local>
2022-04-20 13:12:56 +0100

    updated styling and added pointer and a few other small features to the forms stylig

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,8 +5,8 @@ export default function Form() {
     return (
 <div className="form-container">
         <form className="form">
-            <input type="text" placeholder="Top Text"></input>
-            <input type="text" placeholder="Bottom Text"></input>
+            <input type="text" placeholder="Top Text" className="form-input"></input>
+            <input type="text" placeholder="Bottom Text" className="form-input"></input>
             <button className="form-button">Get a new meme image 🎨</button>
         </form>
 </div>
