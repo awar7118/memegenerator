c729c43c4caedc4a6b54ba178d93479afd08ce27
Ahmed Warsama <ahmedwarsama@Ahmeds-MacBook-Air.local>
2022-04-20 13:40:16 +0100

    progress1

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,12 +2,15 @@ import react from "react";
 import './Form.css'
 
 export default function Form() {
+function MemeHandler(){
+    let memeAPIURL = "https://api.imgflip.com/get_memes"
+}
     return (
 <div className="form-container">
         <form className="form">
             <input type="text" placeholder="Top Text" className="form-input"></input>
             <input type="text" placeholder="Bottom Text" className="form-input"></input>
-            <button className="form-button">Get a new meme image 🎨</button>
+            <button onClick={MemeHandler}className="form-button">Get a new meme image 🎨</button>
         </form>
 </div>
     )
