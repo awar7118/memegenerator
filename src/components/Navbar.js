21e3826c336efa0c9bfab86a219f54523f65f740
Ahmed Warsama <ahmedwarsama@Ahmeds-MacBook-Air.local>
2022-04-20 12:32:22 +0100

    simple navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import react from "react";
+import './Navbar.css'
+import TrollFace from '../images/TrollFace.jpg';
+
+export default function Navbar() {
+    return (
+        <ul>
+            <img src={TrollFace} alt="memegace" className="navbar-meme"></img>
+             <li><a href="#">MemeGenerator</a></li>
+        </ul>
+    )
+}
