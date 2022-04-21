3cf60b4c078d35d1150e35ccb02503a909bf127e
Ahmed Warsama <ahmedwarsama@Ahmeds-MacBook-Air.local>
2022-04-21 22:51:23 +0100

    form and meme updates

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,12 @@
 import react from "react";
 import './Form.css'
-
 export default function Form() {
-function MemeHandler(){
-    let memeAPIURL = "https://api.imgflip.com/get_memes"
-}
     return (
 <div className="form-container">
         <form className="form">
             <input type="text" placeholder="Top Text" className="form-input"></input>
             <input type="text" placeholder="Bottom Text" className="form-input"></input>
-            <button onClick={MemeHandler}className="form-button">Get a new meme image 🎨</button>
+            <button className="form-button">Get a new meme image 🎨</button>
         </form>
 </div>
     )
--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,16 +1,10 @@
-import react, { useState } from 'react';
+import react from 'react';
 import './Meme.css'
 
-const url = "https://api.imgflip.com/get_memes"
-
 export default function Meme() {
-fetch(url)
-    .then(response => response.json())
-    .then(data => console.log(data.data.memes))
-    
     return (
     <div className="meme-container">
-
+Test
     </div>
     )
 }
