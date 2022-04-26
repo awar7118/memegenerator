ccac46dc14f1341c4112a4f8c6c4fccf8bfce0c7
Ahmed Warsama <ahmedwarsama@Ahmeds-MacBook-Air.local>
2022-04-26 09:31:36 +0100

    updated styling

--- src/components/Memes.js
+++ src/components/Memes.js
@@ -65,7 +65,7 @@ export default function Meme() {
                 </button>
             </div>
             <div className="meme">
-                <img src={meme.randomImage} alt="memess" className="meme--image" />
+                <img src={meme.randomImage} alt="memess" className="meme-image" />
                 <h2 className="meme--text top">{meme.topText}</h2>
                 <h2 className="meme--text bottom">{meme.bottomText}</h2>
             </div>
